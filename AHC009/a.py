5a5aac38491fb4dbdf2d4bbae5e3bb00894405ab
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-03-26 16:43:43 +0900

    dijkstra

--- AHC009/a.py
+++ AHC009/a.py
@@ -1,10 +1,60 @@
 from decimal import Decimal
 
+from heapq import heappop, heappush
+
+def dijkstra(graph, start):
+    n = len(graph)
+    dist, parents = [float("inf")] * n, [-1] * n
+    dist[start] = 0
+
+    queue = [(0, start)]
+    while queue:
+        path_len, v = heappop(queue)
+        if path_len == dist[v]:
+            for w, edge_len in graph[v]:
+                if edge_len + path_len < dist[w]:
+                    dist[w], parents[w] = edge_len + path_len, v
+                    heappush(queue, (edge_len + path_len, w))
+
+    return dist, parents
+
 S_I, S_J, T_I, T_J, P = input().split()
 S_I, S_J, T_I, T_J = map(int,(S_I, S_J, T_I, T_J))
 P = Decimal(P)
-H = [list(map(bool,list(input()))) for _ in range(20)]
-V = [list(map(bool,list(input()))) for _ in range(19)]
+H = [list(map(int,list(input()))) for _ in range(20)]
+V = [list(map(int,list(input()))) for _ in range(19)]
+
+graph = [[] for _ in range(20*20)]
+
+# 横の壁チェック
+for i in range(20):
+    for j in range(19):
+
+        if not H[i][j]:
+
+            graph[20*i+j].append([20*i+j+1, 1])
+            graph[20*i+j+1].append([20*i+j, 1])
+
+# 縦の壁チェック
+for i in range(19):
+    for j in range(20):
+        pass
+        if not V[i][j]:
+            graph[20*i+j].append([20*(i+1)+j, 1])
+            graph[20*(i+1)+j].append([20*i+j, 1])
+
+#print(graph)
+
+dist, parents = dijkstra(graph, 20*S_I+S_J)
+
+directions = [20*T_I+T_J]
+while directions[-1] != 20*S_I+S_J:
+    p = parents[directions[-1]]
+    directions.append(p)
+
+#print(dist[20*T_I+T_J], directions)
+directions_ij = [(d//20, d%20) for d in directions[::-1]]
+
 
 answer = ""
 
@@ -17,3 +67,6 @@ for turn in range(200):
 
 print(answer)
 
+
+
+print(H)
