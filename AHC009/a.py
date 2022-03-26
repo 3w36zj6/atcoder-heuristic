caa51f7121a1bc761d1348e04cf0e7a7b8e29c5b
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-03-26 17:16:44 +0900

    複数回dijkstra

--- AHC009/a.py
+++ AHC009/a.py
@@ -45,35 +45,41 @@ for i in range(19):
             graph[20*i+j].append([20*(i+1)+j, 1])
             graph[20*(i+1)+j].append([20*i+j, 1])
 
-#print(graph)
 
-dist, parents = dijkstra(graph, 20*S_I+S_J)
+def search_directions(s_i, s_j):
+    dist, parents = dijkstra(graph, 20*s_i+s_j)
 
-directions = [20*T_I+T_J]
-while directions[-1] != 20*S_I+S_J:
-    p = parents[directions[-1]]
-    directions.append(p)
+    directions = [20*T_I+T_J]
+    while directions[-1] != 20*s_i+s_j:
+        p = parents[directions[-1]]
+        directions.append(p)
 
-#print(dist[20*T_I+T_J], directions)
-directions_ij = [(d//20, d%20) for d in directions[::-1]]
+    directions_ij = [(d//20, d%20) for d in directions]
 
+    return directions_ij
 
+directions_ij = search_directions(S_I, S_J)
+
+mid_ij = directions_ij[len(directions_ij)//2]
 
 answer = []
 
 takahashi_position = [S_I, S_J]
 
 for turn in range(200):
-    answer.append("D")
-
-    if turn < len(directions_ij)-1:
-        pass
-        if directions_ij[turn][0] != directions_ij[turn+1][0]:#縦移動
-            answer[-1] = "D" if directions_ij[turn+1][0] - directions_ij[turn][0] > 0 else "U"
-        elif directions_ij[turn][1] != directions_ij[turn+1][1]:#横移動
-            answer[-1] = "R" if directions_ij[turn+1][1] - directions_ij[turn][1] > 0 else "L"
-
+    answer.append(random.choice("UDDLRR"))
+    #print(turn, len(directions_ij))
+    if len(directions_ij) <= 1:
+        directions_ij = search_directions(*mid_ij)
+        mid_ij = directions_ij[len(directions_ij)//2]
     else:
-        answer[-1] = random.choice("UDLR")
+        pass
+        if directions_ij[-1][0] != directions_ij[-2][0]:#縦移動
+            answer[-1] = "D" if directions_ij[-2][0] - directions_ij[-1][0] > 0 else "U"
+        elif directions_ij[-1][1] != directions_ij[-2][1]:#横移動
+            answer[-1] = "R" if directions_ij[-2][1] - directions_ij[-1][1] > 0 else "L"
+
+        directions_ij.pop()
+
 
 print("".join(answer))
