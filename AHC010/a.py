4a1d9a0b65f83ebdfbd38548ef27bb429e104096
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-04-24 17:04:29 +0900

    右上をちょっとのばす

--- AHC010/a.py
+++ AHC010/a.py
@@ -14,8 +14,8 @@ for x in range(30):
         # if T[i] == 4 or T[i] == 5:
         # if i + 31 < 900 and (T[i] == 2 or T[i] == 4) and (T[i+1] == 1 or T[i+1] == 5) and (T[i+30] == 3 or T[i+30] == 5) and (T[i+31] == 0 or T[i+31] == 4):
         if (
-            x + 2 < 30
-            and y + 2 < 30
+            1 < x < 28
+            and 1 < y < 28
             and (T[x][y] == 2 or T[x][y] == 4)
             and (T[x][y + 1] == 1 or T[x][y + 1] == 5)
             and (T[x + 1][y] == 3 or T[x + 1][y] == 5)
@@ -33,9 +33,8 @@ for x in range(30):
 for loop in mini_loops:
     #print(loop)
     x, y = loop
-    # 左下
+    # 右下
     if T[x+1][y+1] == 4 and not T[x+2][y+1] in (6,7) and not T[x+1][y+2] in (6,7) and not T[x+2][y+2] in (6,7):
-        #print(loop)
 
         ans[30*(x+1)+y+1] = 1
 
@@ -53,5 +52,28 @@ for loop in mini_loops:
             ans[30*(x+2)+y+2] = 1
         elif T[x+2][y+2] <= 3:
             ans[30*(x+2)+y+2] = (3 - T[x+2][y+2]+1)%4
+
+    # 右上
+    if T[x][y+1] == 5 and not T[x-1][y+1] in (6,7) and not T[x][y+2] in (6,7) and not T[x-1][y+2] in (6,7):
+
+        ans[30*(x)+y+1] = 1
+
+
+        if T[x-1][y+1] == 5:
+            ans[30*(x-1)+y+1] = 1
+        elif T[x-1][y+1] <= 3:
+            ans[30*(x-1)+y+1] = (3 - T[x-1][y+1]+3)%4
+
+        if T[x][y+2] == 5:
+            ans[30*(x)+y+2] = 1
+        elif T[x][y+2] <= 3:
+            ans[30*(x)+y+2] = (3 - T[x][y+2]+1)%4
+
+
+        if T[x-1][y+2] == 4:
+            ans[30*(x-1)+y+2] = 1
+        elif T[x-1][y+2] <= 3:
+            ans[30*(x-1)+y+2] = (3 - T[x-1][y+2]+2)%4
+
 
 print("".join(map(str,ans)))
